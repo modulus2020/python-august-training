e5d75fd8ce5564940b4da2894fa8387438b0ad82
modulus2020 <masterjack612@gmail.com>
2025-08-21 15:42:46 +0100

    update

--- python_intro/intro.py
+++ python_intro/intro.py
@@ -1,6 +1,6 @@
 """Topic Today is Syntax = Print Statements, Indentitation, 
 comments, variables, and strings
-"""
+
 print("Hello World!")
 
 print("450")
@@ -21,13 +21,14 @@ print("lipstick")
 
 # Writing Numbers = We write numbers without quotes
 
-""" 
+ 
 What are variables?
 Variables are used to store data values. In Python, variables 
 are created when you assign a value to them. 
 They can hold different types of data, 
 such as numbers, strings, lists, etc. 
-"""
+
+
 # Examples of variables: Name = "Eric", Age = 20
 # Writing Variables = We write variables without quotes, but we assign them values
 
@@ -39,12 +40,12 @@ print("Three is greater than five!")
 if 3 < 5:
     print("Three is less than five!")
 
-""" Some examples of variables """
+Some examples of variables
 name = "Eric"
 age = 20
 print("My name is", name, "and I am", age, "years old.")
 
-""" Assignment 
+ Assignment 
 1. Write a program that stores your name, age and city in a 
 variable and prints them out in one sentnence.
 
@@ -61,9 +62,39 @@ converts them to integers, and prints their:
 
 4. Write a program that takes a string input from the user,
 converts it to uppercase, and prints the result.
-"""
+
 
 first_number = int(input("Enter the first number: "))
 secnd_number = int(input("Enter the second number: "))
 sum = first_number + secnd_number
 print(sum)
+
+
+# DATA TYPES
+    hERE ARE SOME COMMON DATA TYPES IN PYTHON:
+1. Integers: Whole numbers, e.g., 5, -3, 42
+2. Floats: Decimal numbers, e.g., 3.14, -0.001, 2.0
+3. Strings: Text enclosed in quotes, e.g., "Hello", 'Python'    
+4. Booleans: True or False values
+5. Lists: Ordered collections of items, e.g., [1, 2, 3], ["apple", "banana"]
+
+6. Tuples: Immutable ordered collections, e.g., (1, 2, 3), ("a", "b")
+7. Sets: Unordered collections of unique items, e.g., {1, 2, 3}, {"apple", "banana"}
+8. Dictionaries: Key-value pairs, e.g., {"name": "Alice", "age": 30}
+9. None: Represents the absence of a value or a null value  
+10. Complex numbers: Numbers with a real and imaginary part, e.g., 3 + 4j
+"""
+
+fruits = ["apple", "banana", "cherry", "pawpaw", "orange", "pineapple"]
+print(fruits)
+# Accessing elements in a list
+print(fruits[2])
+print(fruits[1])
+print(fruits[1:2])
+print(fruits[1:3])  # Slicing the list to get a sublist
+print(fruits[1:5])
+
+#ASSIGNMENT
+# Write a code that can slice through a list of 10 programming languages.
+# Rewrite that same code using a Tuple
+# Attempt slicing the Tuple like you sliced the List
